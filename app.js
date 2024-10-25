941750ee03aeb92efbd80d790537b2b2988b61f3
LuisAlvarez <luis.alvarezpareja@unicolombo.edu.co>
2024-10-25 15:39:55 -0500

    cambio de puerto

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.post('/cancel-send', (req, res) => {
 QRPortalWeb();
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000; // Usa el puerto que Railway asigna automáticamente
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
