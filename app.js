b3f9ca462b1bf1e99b4e08ffc2caf81fdddb1aa0
LuisAlvarez <luis.alvarezpareja@unicolombo.edu.co>
2024-10-25 15:24:01 -0500

    cambio de puerto

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.post('/cancel-send', (req, res) => {
 QRPortalWeb();
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
