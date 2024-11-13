83c9f8ce0538a212a5cf8570b3f6cf696352dec3
LuisAlvarez <luis.alvarezpareja@unicolombo.edu.co>
2024-11-13 10:13:43 -0500

    update PORT

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.post('/cancel-send', (req, res) => {
 QRPortalWeb();
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
